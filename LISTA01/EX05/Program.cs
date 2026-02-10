751043491949c42cdab15669bf9b3c1c7d88118f
lavinia-alves <laviniaalves@ucl.br>
2026-02-10 20:50:12 -0300

    Exercício 5
    Implemented a multiplication table for the number 1 from 1 to 10.

--- LISTA01/EX05/Program.cs
+++ LISTA01/EX05/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//5.
+//tabuada 1 ao 10
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //declarando variáveis 
+        int num=1, tabuada;
+        Console.WriteLine("Tabuada do 1");
+        //cálculo da tabuada
+        for (int i=1; i<=10; i++)
+        {
+          tabuada = num * i;
+          Console.WriteLine(num + "x"  + i+" = " +tabuada); 
+        }
+    }
+}
