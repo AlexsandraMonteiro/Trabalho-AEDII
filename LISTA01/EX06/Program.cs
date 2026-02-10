899b4ef444102d10d49553ca1528419b4b142b4c
lavinia-alves <laviniaalves@ucl.br>
2026-02-10 20:45:54 -0300

    Exercícios
    Added functionality to count the digits of a number input by the user.

--- LISTA01/EX06/Program.cs
+++ LISTA01/EX06/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+6.
+//contador de dígitos 
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //declarando variáveis 
+        int num, cont=0;
+        //pedidndo entradas
+        Console.WriteLine("Informe um número");
+        num = int.Parse(Console.ReadLine());
+        //contando a quantidade de dígitos do número
+        // Verificando se o número é 0
+        if (num == 0)
+        {
