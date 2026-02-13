e3486e2389a542f24d504c963d71ab0731993598
Laís <lais.sfalqueto@gmail.com>
2026-02-13 15:36:12 -0300

    Repetir o nome 100 vezes
    ..

--- LISTA03/EX01-fácil/Program.cs
+++ LISTA03/EX01-fácil/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome");
+            string nome = Console.ReadLine();
+
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("{0}° - {1}",i, nome);
+            }
+
+        }
+    }
+}
