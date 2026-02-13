bc22a5f792827deb07351ea3f0fe555902c42beb
lavinia-alves <laviniaalves@ucl.br>
2026-02-13 11:53:00 -0300

    Exercício 5
    Updated the program to prompt the user for a number to multiply and display its multiplication table.

--- LISTA01/EX05/Program.cs
+++ LISTA01/EX05/Program.cs
@@ -1,5 +1,5 @@
 //5.
-//tabuada 1 ao 10
+//Construa a tabela de multiplicação de números de 1 a 10 (ex.: 1 x 1 = 1, 1 x 2 = 2, etc.).
 using System;
 
 public class HelloWorld
@@ -7,8 +7,11 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         //declarando variáveis 
-        int num=1, tabuada;
-        Console.WriteLine("Tabuada do 1");
+        int num, tabuada;
+        //pedindo o número que vai ser multiplicado
+        Console.WriteLine("Digite o número que deseja multiplicar");
+        num = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Tabuada do {num}");
         //cálculo da tabuada
         for (int i=1; i<=10; i++)
         {
