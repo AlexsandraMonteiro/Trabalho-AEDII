3d90aae818ad12abf7ede50f29fc905a0f6abdd7
Laís <lais.sfalqueto@gmail.com>
2026-02-13 16:24:26 -0300

    Sequencia de Fibonacci
    Implement Fibonacci sequence generation up to 30.

--- LISTA03/EX04-fácil/Program.cs
+++ LISTA03/EX04-fácil/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 0;
+        int b = 1; // define os dois primeiros numeros
+
+        Console.WriteLine("Números da sequencia de Fibonacci:");
+        Console.WriteLine(a);
+        Console.WriteLine(b); // escreve os dois primeiros numeros
+
+        for (int i = 3; i <= 30; i++) // repete o i até o 30
+        {
+            int prox = a + b;
+            Console.WriteLine(prox); // escreve na tela o resultado do calculo dos numeros antecessores
+
+            a = b;
+            b = prox; // substitui os numeros, a pelo b e b pelo proximo para somar outra vez
+        }
+    }
+}
