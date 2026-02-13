5bdd070e50f72f51d6facca762994ac4376d8527
Laís <lais.sfalqueto@gmail.com>
2026-02-13 16:12:39 -0300

    programa que imprima na tela um triângulo de “*”

--- LISTA03/EX03-fácil/Program.cs
+++ LISTA03/EX03-fácil/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 4; i++) // controla a quantidade de linhas
+        {
+            for (int j = 1; j <= i; j++) // controla a quantidade de *
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(); // pula uma linha
+        }
+    }
+}
