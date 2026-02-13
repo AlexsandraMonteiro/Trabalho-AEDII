218501416df34b1a99ad84c931938f209bea13f6
Laís <lais.sfalqueto@gmail.com>
2026-02-13 15:57:16 -0300

    Update Program.cs

--- LISTA03/EX01-fácil/Program.cs
+++ LISTA03/EX01-fácil/Program.cs
@@ -1,12 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace trabalho1
-{
-    internal class Program
+class Program
     {
         static void Main(string[] args)
         {
@@ -20,4 +14,3 @@ namespace trabalho1
 
         }
     }
-}
