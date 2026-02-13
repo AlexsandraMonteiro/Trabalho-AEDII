1a49117c8bd7ffd31af59326adda80b13be04ec3
Laís <lais.sfalqueto@gmail.com>
2026-02-13 15:43:39 -0300

    repete o nome 100 vezes

--- LISTA03/EX01-fácil/Program.cs
+++ LISTA03/EX01-fácil/Program.cs
@@ -11,11 +11,11 @@ namespace trabalho1
         static void Main(string[] args)
         {
             Console.WriteLine("Digite seu nome");
-            string nome = Console.ReadLine();
+            string nome = Console.ReadLine(); // lê o nome que você digitar
 
-            for(int i = 1; i <= 100; i++)
+            for(int i = 1; i <= 100; i++) // repete o i de 1 até 100
             {
-                Console.WriteLine("{0}° - {1}",i, nome);
+                Console.WriteLine("{0}° - {1}",i, nome); // escreve o nome escrito no programa na tela 100 vezes
             }
 
         }
