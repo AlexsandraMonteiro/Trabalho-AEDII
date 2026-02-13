aad3e632416f289a9fca89a497f8feff695d08cc
Laís <lais.sfalqueto@gmail.com>
2026-02-13 15:51:25 -0300

    programa que exiba os números pares entre 1 e 100

--- LISTA03/EX02-fácil/Program.cs
+++ LISTA03/EX02-fácil/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        for (int i = 1; i<= 100; i++) // repete o i de 1 até 100
+            {
+                if(i % 2 == 0) // divide o i por 2 e apenas mostra os numeros que o resultado da divisão for igual a 0
+                {
+                    Console.WriteLine(i); // escreve os números na tela
+                }
+            }
+        }
+    }
+}
