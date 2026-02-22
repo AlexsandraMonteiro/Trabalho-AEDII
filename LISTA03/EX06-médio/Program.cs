d61375684f758d7e7eabd4c406deee4241902fd6
lavinia-alves <laviniaalves@ucl.br>
2026-02-22 20:46:55 -0300

    Exercício 6

--- LISTA03/EX06-médio/Program.cs
+++ LISTA03/EX06-médio/Program.cs
@@ -6,3 +6,65 @@
 
 * Um construtor que inicializa o ponto através das coordenadas de um outro ponto recebido como parâmetro.
 */
+using System;
+class Ponto 
+{
+ private double x;
+ private double y;
+ //construtor sem parmetros
+ public Ponto()
+ {
+     x = 0;
+     y = 0;
+ }
+ //Construtor com parâmetros
+ public Ponto(Ponto outroPonto)
+ {
+     this.x = outroPonto.x;
+     this.y = outroPonto.y;
+ }
+ //contrutor que copia outro ponto
+ public Ponto (double x, double y)
+ {
+     this.x = x;
+     this.y = y;
+ }
+ //propriedades
+ public double X
+ {
+     get { return x; }
+     set { x = value; }
+ }
+ public double Y 
+ {
+     get { return y; }
+     set { y = value; }
+ }
+ public void Exibir ()
+ {
+   Console.WriteLine($"({x}, {y})");
+ }
+}
+class Programa
+{
+    static void Main(string[] args)
+    {
+        // Usando construtor vazio
+        Ponto p1 = new Ponto();
+
+        // Usando construtor com parâmetros
+        Ponto p2 = new Ponto(5, 8);
+
+        // Usando construtor que copia outro ponto
+        Ponto p3 = new Ponto(p2);
+
+        Console.WriteLine("Ponto 1:");
+        p1.Exibir();
+
+        Console.WriteLine("Ponto 2:");
+        p2.Exibir();
+
+        Console.WriteLine("Ponto 3 (cópia do ponto 2):");
+        p3.Exibir();
+    }
+}
