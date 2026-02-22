482c0e1b13a7bd717c009223626cb2ff218af303
lavinia-alves <laviniaalves@ucl.br>
2026-02-22 10:28:58 -0300

    Exercício 22

--- LISTA01/EX22/Program.cs
+++ LISTA01/EX22/Program.cs
@@ -1,3 +1,93 @@
 ﻿/* 22. Faça	uma	programa	para	representar	a	árvore	genealógica	de	uma	família.	
 Para	tal,	crie	uma	classe	Pessoa	que	permita	indicar,	além	de	nome	e	idade,	o	pai	e	a	mãe.	
 Tenha	em	mente	que	pai	e	mãe	também	são	do	tipo	Pessoa.	*/
+using System;
+class Pessoa
+{
+    private string nome;
+    private int idade;
+    private Pessoa pai;
+    private Pessoa mae;
+    
+     public Pessoa()
+     {
+         nome = "Sem nome";
+         idade = 0;
+         pai = null;
+         mae = null;
+     }
+     //construtor
+     public Pessoa(string nome, int idade)
+     {
+         this.nome = nome;
+         this.idade = idade;
+         this.pai = null;
+         this.mae = null;
+     }
+     //propriedades
+     public string Nome 
+     {
+         get { return nome; }
+         set { nome = value; }
+     }
+     public int Idade
+     {
+         get { return idade; }
+         set { idade = value; }
+     }
+     public Pessoa Pai
+     {
+         get { return pai; }
+         set { pai = value; }
+     }
+     public Pessoa Mae 
+     {
+         get { return mae; }
+         set { mae = value; }
+     }
+     //metodod para exibir dados 
+     public void ExibirDados()
+     {
+         Console.WriteLine ($"Nome: {nome}");
+         Console.WriteLine ($"Idade: {idade}");
+         if (pai != null)
+         {
+              Console.WriteLine ($"Pai: {Pai.Nome}");
+         }
+         else
+         {
+            Console.WriteLine ("Pai não informado");
+         }
+         if(mae != null)
+         {
+             Console.WriteLine($"Mãe: {Mae.Nome}");
+         }
+         else
+         {
+             Console.WriteLine("Mãe não informada");
+         }
+     }
+}
+class Progama 
+{
+    static void Main (string [] args)
+    {
+        // Criando os pais
+        Pessoa pai = new Pessoa("Carlos", 50);
+        Pessoa mae = new Pessoa("Maria", 48);
+
+        // Criando o filho
+        Pessoa filho = new Pessoa("João", 20);
+
+        // Ligando pai e mãe ao filho
+        filho.Pai = pai;
+        filho.Mae = mae;
+
+        // Exibindo dados
+        Console.WriteLine("Árvore Genealógica:\n");
+
+        filho.ExibirDados();
+        pai.ExibirDados();
+        mae.ExibirDados();
+    }
+}
